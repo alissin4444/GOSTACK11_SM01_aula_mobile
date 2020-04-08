90c257e429a6ba6a747b9802d3bf6aa27b03a0ee
alissin <alissin@pop-os.localdomain>
2020-04-08 16:13:04 -0300

    Class finalized and I can create my apps now

---
+++ src/index.js
@@ -0,0 +1,125 @@
+import React, { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  FlatList,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  StatusBar,
+} from "react-native";
+
+import api from "./services/api";
+
+export default function App() {
+  const [projects, setProjects] = useState([
+    {
+      id: 1,
+      title: "Umbriel",
+    },
+    {
+      id: 2,
+      title: "Nodejs",
+    },
+    {
+      id: 3,
+      title: "Reactjs",
+    },
+    {
+      id: 4,
+      title: "React Native",
+    },
+    {
+      id: 5,
+      title: "TypeORM",
+    },
+    {
+      id: 6,
+      title: "Adonisjs",
+    },
+    {
+      id: 7,
+      title: "Expressjs",
+    },
+    {
+      id: 8,
+      title: "Sequelize",
+    },
+  ]);
+
+  useEffect(() => {
+    api.get("projects").then((response) => setProjects(response.data));
+  }, []);
+
+  async function handleAddProject() {
+    const response = await api.post("projects", {
+      title: `new repo ${new Date()}`,
+      owner: "Alissin Santos",
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="#dfe6e9" />
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.title}>Projects</Text>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({ item: project }) => (
+            <Text style={styles.project} key={project.id}>
+              {project.title}
+            </Text>
+          )}
+        />
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Adicionar repositório</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#dfe6e9",
+    flex: 1,
+    paddingTop: 40,
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    marginBottom: 10,
+    color: "#2d3436", // #576574
+  },
+  project: {
+    fontSize: 14,
+    marginBottom: 5,
+    color: "#576574",
+  },
+  button: {
+    height: 50,
+    backgroundColor: "#2d3436",
+    borderRadius: 25,
+    margin: 20,
+    marginBottom: 30,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#bdc3c7",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    fontSize: 14,
+  },
+});
